9b7c7208d4525bec54185b117e4cf3c7477484b6
Boris Morel <Boris.Morel@imag.fr>
2013-01-17 15:38:03 +0100

    Adding logger

--- Manager/ZipManager.php
+++ Manager/ZipManager.php
@@ -9,16 +9,19 @@ class ZipManager
         $_archive
         ;
 
-    public function __construct()
+    public function __construct(\Symfony\Bridge\Monolog\Logger $logger)
     {
+        $this->logger = $logger;
         $this->_archive = new \ZipArchive();
-        
+                
         $zipPath = '/tmp/'.uniqId().'.zip';
         $this->_archive->open($zipPath, \ZIPARCHIVE::CREATE);
     }
 
     public function add($path)
     {
+        $this->logger->info(sprintf('Adding file %s to zip archive', $path));
+
         $this->_archive->addFile($path, basename($path));
     }
 
