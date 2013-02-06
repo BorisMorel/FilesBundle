24ad587bd37b5d61209c55253e0e9603f1c1cedf
Boris Morel <Boris.Morel@imag.fr>
2013-02-06 15:57:54 +0100

    The pdfs files can be prefixed into the zip archive

--- Manager/ZipManager.php
+++ Manager/ZipManager.php
@@ -6,23 +6,54 @@ class ZipManager
 {
     private
         $logger,
-        $_archive
+        $zipPrefix = 'file',
+        $fileDirPrefix = 'file',
+        $_archive = null
         ;
 
     public function __construct(\Symfony\Bridge\Monolog\Logger $logger)
     {
         $this->logger = $logger;
+    }
+
+    public function init()
+    {
         $this->_archive = new \ZipArchive();
-                
-        $zipPath = '/tmp/'.uniqId().'.zip';
+        $zipPath = '/tmp/'.$this->zipPrefix.'-'.uniqId().'.zip';
         $this->_archive->open($zipPath, \ZIPARCHIVE::CREATE);
+        
+        return $this;
+    }
+
+    public function setZipPrefix($prefix)
+    {
+        $this->zipPrefix = $prefix;
+
+        return $this;
+    }
+
+    public function getZipPrefix()
+    {
+        return $this->zipPrefix;
+    }
+
+    public function addFileDirectoryPrefix($dirname)
+    {
+        $this->fileDirPrefix = trim($dirname, '/');
     }
 
     public function add($path)
     {
+        if (null === $this->_archive) {
+            $this->init();
+        }
+
         $this->logger->info(sprintf('Adding file %s to zip archive', $path));
 
-        $this->_archive->addFile($path, basename($path));
+        
+        $this->_archive->addFile($path, $this->fileDirPrefix.'/'.basename($path));
+
+        return $this;
     }
 
     public function getZip()
