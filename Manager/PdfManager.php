4b9244ed3e46b7a856bd1bf2da4d0282dc956a9c
Boris Morel <Boris.Morel@imag.fr>
2013-10-04 08:51:29 +0200

    Fix to support php5.4

--- Manager/PdfManager.php
+++ Manager/PdfManager.php
@@ -199,12 +199,12 @@ class PdfManager
                 throw new \RuntimeException(sprintf('The conversion PS to PDF process has failed: %s', $process->getErrorOutput()));
             }
             
+            unlink($psFile);
             return $pdfTempFile;
             
         } catch (\Exception $e) {
-            throw $e;
-        } finally {
             unlink($psFile);
+            throw $e;
 
         }
     }
